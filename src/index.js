1ce1fc87da7d283c5f7c1914823733e192f412ec
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-12-11 10:09:40 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ breedSelect.addEventListener('change', event => {
       Notiflix.Notify.warning(
         '❌ Oops! Something went wrong! Try reloading the page!',
         {
-          //  position: 'center-top' ,
+          position: 'center-top',
         }
       );
     })
@@ -80,71 +80,3 @@ breedSelect.addEventListener('change', event => {
       toggleLoader('none');
     });
 });
-
-// -----------другий розвязок прошу не брати його під увагу
-// import { fetchBreeds, fetchCatByBreed } from './cat-api';
-// // import Notiflix from 'notiflix';
-// import SlimSelect from 'slim-select';
-
-// const select = document.querySelector('.breed-select');
-// const loader = document.querySelector('.loader');
-// const divPicture = document.querySelector('.div-picture');
-// const catInfo = document.querySelector('.cat-info');
-
-// fetchBreeds()
-//   .then(breeds => {
-//     console.log('jcm lt', breeds);
-//     select.style.visibility = 'visible';
-//     // loader.style.display = 'none';
-//     // const cat = breeds
-//     //   .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
-//     //   .join('');
-
-//     select.insertAdjacentHTML('beforeend', createMarkup(breeds.data));
-//   })
-
-//   .catch(error => {
-//     console.log(error);
-//     loader.style.display = 'none';
-//     // Notiflix.Notify.failure(
-//     //   'Oops! Something went wrong! Try reloading the page!'
-//     // );
-//   });
-// fetchBreeds();
-
-// select.addEventListener('change', hendelSelect);
-// divPicture.innerHTML = '';
-// catInfo.innerHTML = '';
-// function hendelSelect(event) {
-//   event.preventDefault();
-//   const selectedBreed = this.value;
-
-//   loader.style.display = 'block';
-
-//   fetchCatByBreed(selectedBreed).then(breeds => {
-//     // loader.style.display = 'none';
-//     const catData = breeds[0];
-//     divPicture.innerHTML = `
-//     <img class="picture" src="${catData.url}" alt="${catData.breeds[0].name}">`;
-//     catInfo.innerHTML = `
-//       <h2 class="title">${catData.breeds[0].name}</h2>
-//       <p class="text">${catData.breeds[0].description}</p>
-//       <p class="temp"><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>`;
-//   });
-// }
-
-// function createMarkup(arr) {
-//   return arr
-
-//     .map(
-//       ({ name, image, temperament, description }) => `
-//     <div class="cat-info">
-//       <p class="cat-name">${name}</p>
-//       <img class="div-picture" src="${image}" alt="${name}" />
-//       <p class="cat-temp">${temperament}</p>
-//       <p class="cat-descr">${description}</p>
-//     </div>
-//   `
-//     )
-//     .join('');
-// }
